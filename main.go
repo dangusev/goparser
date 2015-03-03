01d06781cbcb9ccc4934a1cabcfd8561ba535d42
Dan Gusev <d.gusev@netrika.ru>
2015-03-04 01:40:29 +0300

    per-page parsing in progress

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 
 	"github.com/blasyrkh123/goparser/models"
 	"github.com/moovweb/gokogiri"
@@ -11,27 +14,51 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-func getData(url string, session *mgo.Session) bool {
+func makeRequest(url string) []byte {
 	response, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	body, err := ioutil.ReadAll(response.Body)
 	defer response.Body.Close()
+	return body
+}
 
-	root, _ := gokogiri.ParseHtml(body)
+func getData(url string, session *mgo.Session) {
+
+	root, _ := gokogiri.ParseHtml(makeRequest(url))
 	defer root.Free()
 	defer session.Close()
 
 	data, _ := root.Search("//div[contains(@class,\"item\")][@data-type=\"1\"]/div[@class=\"description\"]")
 	for _, item := range data {
 		header, _ := item.Search(item.Path() + "/h3[@class=\"title\"]/a")
+		item := models.Item{
+			Title: strings.Trim(strings.TrimSpace(header[0].Content()), "\n"),
+			Url:   header[0].Attribute("href").Content(),
+		}
 
-		item := models.Item{Title: header[0].Content(), Url: header[0].Attribute("href").Content()}
-
-		session.DB("goparser").C("items").Insert(&models.Item{Url: item.Url, Title: "test title"})
+		_, err := session.DB("goparser").C("items").Upsert(bson.M{"url": item.Url}, &models.Item{Url: item.Url, Title: item.Title})
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	return true
+}
+
+func getPagesCount(pageURL string) (count int64) {
+	count = 1
+	root, _ := gokogiri.ParseHtml(makeRequest(pageURL))
+	elem, err := root.Search("//a[text()=\"Последняя\"][@class=\"pagination__page\"]")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(elem) > 0 {
+		lastPageURL := elem[0].Attribute("href").Content()
+		u, _ := url.Parse(lastPageURL)
+		q, _ := url.ParseQuery(u.RawQuery)
+		count, _ = strconv.ParseInt(q.Get("p"), 10, 64)
+	}
+	return
 }
 
 func main() {
@@ -44,16 +71,17 @@ func main() {
 	queries := session.DB("goparser").C("queries")
 
 	queries.Find(bson.M{}).All(&results)
-	for _, query := range results {
-		// TODO: think about goroutines
-		getData(query.Url, session.Clone())
-	}
+	// for _, query := range results {
+	print(getPagesCount("https://www.avito.ru/sankt-peterburg/zapchasti_i_aksessuary/zapchasti/dlya_avtomobiley?i=1&q=3"))
+	// TODO: think about goroutines
+	// getData("https://www.avito.ru/sankt-peterburg/zapchasti_i_aksessuary/zapchasti/dlya_avtomobiley?i=1&q=3", session.Clone())
+	// }
 
-	// getData("https://www.avito.ru/sankt-peterburg/zapchasti_i_aksessuary/zapchasti/dlya_avtomobiley/rulevoe_upravlenie?i=1&q=308+%D0%BF%D0%B5%D0%B6%D0%BE+%D1%80%D1%83%D0%BB%D1%8C&s=1")
 }
 
 //TODO:
-// Переход по страницам
+// Переход по страницам, парсинг нескольких страниц одновременно (таймаут 0.1, 10 за раз)
 // Запись результатов поиска в БД
 // Работа с запросами (CRUD)
 // Уведомления
+// "https://www.avito.ru/sankt-peterburg/zapchasti_i_aksessuary/zapchasti/dlya_avtomobiley/rulevoe_upravlenie?i=1&q=308+%D0%BF%D0%B5%D0%B6%D0%BE+%D1%80%D1%83%D0%BB%D1%8C&s=1"
