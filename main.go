867d0e829de59869936b283e27f0259d8e6acbd9
Gusev Daniil <d.gusev@netrika.ru>
2015-03-03 19:33:24 +0300

    Some base CRUD operations

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 
 	"github.com/blasyrkh123/goparser/models"
 	"github.com/moovweb/gokogiri"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func getData(url string) bool {
+func getData(url string, session *mgo.Session) bool {
 	response, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -19,6 +21,7 @@ func getData(url string) bool {
 
 	root, _ := gokogiri.ParseHtml(body)
 	defer root.Free()
+	defer session.Close()
 
 	data, _ := root.Search("//div[contains(@class,\"item\")][@data-type=\"1\"]/div[@class=\"description\"]")
 	for _, item := range data {
@@ -26,14 +29,27 @@ func getData(url string) bool {
 
 		item := models.Item{Title: header[0].Content(), Url: header[0].Attribute("href").Content()}
 
-		print(item.Url)
-
+		session.DB("goparser").C("items").Insert(&models.Item{Url: item.Url, Title: "test title"})
 	}
 	return true
 }
 
 func main() {
-	getData("https://www.avito.ru/sankt-peterburg/zapchasti_i_aksessuary/zapchasti/dlya_avtomobiley/rulevoe_upravlenie?i=1&q=308+%D0%BF%D0%B5%D0%B6%D0%BE+%D1%80%D1%83%D0%BB%D1%8C&s=1")
+	var results []models.Query
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+	queries := session.DB("goparser").C("queries")
+
+	queries.Find(bson.M{}).All(&results)
+	for _, query := range results {
+		// TODO: think about goroutines
+		getData(query.Url, session.Clone())
+	}
+
+	// getData("https://www.avito.ru/sankt-peterburg/zapchasti_i_aksessuary/zapchasti/dlya_avtomobiley/rulevoe_upravlenie?i=1&q=308+%D0%BF%D0%B5%D0%B6%D0%BE+%D1%80%D1%83%D0%BB%D1%8C&s=1")
 }
 
 //TODO:
--- models/models.go
+++ models/models.go
@@ -1,12 +1,18 @@
 package models
 
+import "gopkg.in/mgo.v2/bson"
+
 // Item from site
 type Item struct {
-	Id         int64
-	Title, Url string
+	Id    bson.ObjectId `bson:"_id,omitempty"`
+	Title string        `bson: "title"`
+	Url   string        `bson: "url"`
 }
 
 // Query from site
 type Query struct {
-	Text string
+	Id  bson.ObjectId `bson:"_id,omitempty"`
+	Url string        `bson:"url"`
 }
+
+// func (Query q) Map()
